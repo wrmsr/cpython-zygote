9cb5d7d6267e08c5f4b4565da0cadff9208eab7a
wrmsr <timwilloney@gmail.com>
2015-09-03 16:22:45 -0700

    --

--- Lib/zygote/wsgi.py
+++ Lib/zygote/wsgi.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-import abc
 import logging
 
 from . import server
@@ -13,9 +12,18 @@ log = logging.getLogger(__name__)
 class WsgiZygoteServer(server.ZygoteServer):
     __metaclass__ = abc.ABCMeta
 
-    @abc.abstractmethod
-    def __call__(self, environ, start_response):
-        raise NotImplementedError()
+    def __init__(self, path, app, **kwargs):
+        super(WsgiZygoteServer, self).__init__(path, **kwargs)
+
+    def work(self):
+        while True:
+            environ = self.writeobj()
+            def start_response(status, response_headers, exc_info=None):
+                self.writeobj(status)
+                self.writeobj(response_headers)
+                self.writeobj(exc_info)
+            for buf in self.application(environ, start_response):
+                self.write(buf)
 
 
 class WsgiZygoteClient(server.ZygoteClient):
