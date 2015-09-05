1dd8f2b6e24eaa005fd525d5bf0feb875685512b
wrmsr <timwilloney@gmail.com>
2015-09-05 02:44:44 -0700

    --

--- Lib/zygote/common.py
+++ Lib/zygote/common.py
@@ -6,7 +6,7 @@ import marshal
 import types
 
 
-def raw_import(name, pyc_path):
+def raw_import(pyc_path, name, package=None, builtins=None):
     if not pyc_path.endswith('.pyc'):
         raise ValueError(pyc_path)
     with open(pyc_path, 'rb') as f:
@@ -14,6 +14,12 @@ def raw_import(name, pyc_path):
     c = marshal.loads(b[8:])
     m = types.ModuleType(name)
     sys.modules[name] = m
-    m.__dict__['__file__'] = pyc_path
+    m.__dict__.update(
+        __builtins__=builtins if builtins is not None else __builtins__,
+        __file__=pyc_path,
+        __name__=name,
+        __package__=package,
+        __doc__=None
+    )
     exec c in m.__dict__
     return m
