b131a42a0d2c00b14920921cdd2cef4ca45e4a01
wrmsr <timwilloney@gmail.com>
2015-09-04 17:19:07 -0700

    --

---
+++ Lib/zygote/common.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import sys
+import marshal
+import types
+
+
+def manual_import(name, pyc_path):
+    if not pyc_path.endswith('.pyc'):
+        raise ValueError(pyc_path)
+    with open(pyc_path, 'rb') as f:
+        b = f.read()
+    c = marshal.loads(b[8:])
+    m = types.ModuleType(name)
+    sys.modules[name] = m
+    m.__dict__['__file__'] = f
+    exec c in m.__dict__
+    return m
